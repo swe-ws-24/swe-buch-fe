7892a975087e3549d3bade071bc67e339260cebc
Leon Gauweiler <leon.gauweiler@gmail.com>
2024-06-11 19:52:44 +0200

    added: bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import BootstrapClient from '@/components/BootstrapClient';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <BootstrapClient />
     </html>
   );
 }
