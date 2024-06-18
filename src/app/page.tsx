424d6e56e7ad5ed2ead01e2c93760af189b963e6
malikbredehoeft <131286836+malikbredehoeft@users.noreply.github.com>
2024-06-18 17:56:29 +0200

    Updated: Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@ import Sidebar from "@/components/searchPage/Sidebar"
 export default function Home() {
   return (
     <main>
-      <div>
-        <Sidebar />
+      <div className="container-fluid vh-100">
+        <div className="row">
+          <div className="col-md-3 p-0 vh-100">
+            <Sidebar />
+          </div>
+          <div className="col-md-9">
+            <p>test</p>
+          </div>
+        </div>
       </div>
     </main>
   )
