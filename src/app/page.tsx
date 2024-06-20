23929d0aab496677b6d9fb5c3381b972e5b391c4
malikbredehoeft <131286836+malikbredehoeft@users.noreply.github.com>
2024-06-20 11:02:17 +0200

    Updated:  page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
-
+import Sidebar from "@/components/searchPage/Sidebar";
+import Anlegebutton from '../components/searchPage/Anlegebutton';
+import Searchbar from "@/components/searchPage/Searchbar";
+import BookList from "@/components/searchPage/Booklist"; // Import the Booklist component
 
 export default function Home() {
   return (
     <main>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2 p-0">
+                    <Sidebar />
+                </div>
+                <div className="col-md-10">
+                    <div className="row">
+                        <div className="col-10 p-3">
+                            <Searchbar />
+                        </div>
+                        <div className="col-2">
+                            <Anlegebutton />
+                        </div>
+                        <div className="col-12">
+                            <BookList />
+                        </div>
 
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </main>
   )
 }
