5430602204e77e42f8ad47c18c639a8216f32c0d
malikbredehoeft <131286836+malikbredehoeft@users.noreply.github.com>
2024-06-26 16:17:05 +0200

    Updated: Anlegebutton

--- src/components/searchPage/Anlegebutton.tsx
+++ src/components/searchPage/Anlegebutton.tsx
@@ -1,12 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Anlegebutton = () => {
           return (
             <div className="container mt-3">
             <form className="d-flex align-items-center">
-              <button type="submit" className="btn btn-primary ms-2">
+              <Link href="/admin/adminPanel" type="submit" className="btn btn-primary ms-2">
                 + Hinzufügen
-              </button>
+              </Link>
             </form>
           </div>
         );
